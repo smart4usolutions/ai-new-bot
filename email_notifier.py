7bac8c1040e68bc8e3c54efc321ca6546eaa11b9
smart4usolutions <smart4usolutions@gmail.com>
2026-03-16 12:28:24 +0530

    email fixed

--- email_notifier.py
+++ email_notifier.py
@@ -3,26 +3,25 @@ import os
 
 def send_email(status, message):
 
+    smtp_user = os.getenv("SMTP_USER")
+    smtp_pass = os.getenv("SMTP_PASS")
+
+    yag = yagmail.SMTP(
+        user=smtp_user,
+        password=smtp_pass,
+        host="smtp-relay.brevo.com",
+        port=587
+    )
+
+    subject = f"AI News Bot Status: {status}"
+
     try:
-        smtp_user = os.getenv("SMTP_USER")
-        smtp_pass = os.getenv("SMTP_PASS")
-
-        yag = yagmail.SMTP(
-            user=smtp_user,
-            password=smtp_pass,
-            host="smtp-relay.brevo.com",
-            port=587
-        )
-
-        subject = f"AI News Bot Status: {status}"
-
         yag.send(
             to=smtp_user,
             subject=subject,
             contents=message
         )
+        print("Email sent successfully")
 
-        print("Email sent")
-        
     except Exception as e:
-        print("Email error:", e)
+        print("Email sending failed:", e)
