6636f59ed1404d33d6f61108c55f53d1c0a765d5
smart4usolutions <smart4usolutions@gmail.com>
2026-03-16 12:12:22 +0530

    fixed pipeline error

--- run_pipeline.py
+++ run_pipeline.py
@@ -13,15 +13,16 @@ steps = [
     "python news_template.py",
     "python shorts_video_generator.py",
     "python youtube_uploader.py"
-    send_email(
-        "SUCCESS",
-        "AI news bot ran successfully and uploaded video."
-    )
 
 ]
 
 for step in steps:
     print(f"\n🚀 Running: {step}\n")
     subprocess.run(step, shell=True)
+
+send_email(
+    "SUCCESS",
+    "AI news bot ran successfully and uploaded video."
+)
 
 print("\n✅ ALL SHORTS UPLOADED SUCCESSFULLY")
