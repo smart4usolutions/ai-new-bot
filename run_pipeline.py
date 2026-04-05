8073fe87df2f26aef436117eeb1e66c0f3a9b720
smart4usolutions <smart4usolutions@gmail.com>
2026-04-05 17:57:01 +0530

    test mode

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,8 +12,8 @@ steps = [
     "python news_filter.py",
     "python image_downloader.py",
     "python news_template.py",
-    #"python script_generator.py",
-    #"python voice_generator.py",
+    "python script_generator.py",
+    "python voice_generator.py",
     # "python subtitle_generator.py",
     #"python shorts_video_generator.py",
     #"python youtube_uploader.py"
